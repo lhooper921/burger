90d6013b72cade5ebeec5efaa60f925f33888521
Lauren Hooper <lluther921@gmail.com>
2020-10-03 22:56:43 -0700

    html showing on page

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+-- Create the table burgers.
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT,
+  burger_name varchar(45) NOT NULL,
+  devoured boolean NOT NULL,
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records into burgers.
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", True);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Double Cheeseburger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", True);
